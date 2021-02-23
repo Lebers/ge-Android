f6a0a7a2fa5e187d0e08b75acf3f1a6c6b5f81cf
servicios.lebers <servicios.lebers@gmail.com>
2021-02-23 01:29:13 -0600

    ups

---
+++ latest.php
@@ -0,0 +1,21 @@
+<?php
+
+$fileName = basename('0.23.2.1.apk');
+$filePath = 'files/'.$fileName;
+if(!empty($fileName) && file_exists($filePath)){
+    // Define headers
+    header("Cache-Control: public");
+    header("Content-Description: File Transfer");
+    header("Content-Disposition: attachment; filename=$fileName");
+    header("Content-Type: application/zip");
+    header("Content-Transfer-Encoding: binary");
+
+    // Read the file
+    readfile($filePath);
+    exit;
+}else{
+    echo 'The file does not exist.';
+}
+
+?>
+
